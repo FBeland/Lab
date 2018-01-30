ec95e69c6924afef023c9c51ef5ff55c7b2b837b
Frédéric Béland <frederic@umaknow.com>
2018-01-29 22:23:14 -0500

    Lab-x4 initialisation.

---
+++ Lab-x4/src/webparts/soleilVert/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISoleilVertWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SoleilVertWebPartStrings' {
+  const strings: ISoleilVertWebPartStrings;
+  export = strings;
+}
